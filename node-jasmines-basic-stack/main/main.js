ac57463aee553bb08803738d28119815eb470369
lzjjj <1358999379@qq.com>
2018-07-07 23:02:57 +0800

    [louis] Migrate the code to

--- node-jasmines-basic-stack/main/main.js
+++ node-jasmines-basic-stack/main/main.js
@@ -1,12 +1,169 @@
-// Write your cade below:
-function caclRemaindar() {};
+'use strict';
 
-function caclSum() {};
+function loadAllItems() {
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: '可口可乐',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: '雪碧',
+      unit: '瓶',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: '苹果',
+      unit: '斤',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: '荔枝',
+      unit: '斤',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: '电池',
+      unit: '个',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: '方便面',
+      unit: '袋',
+      price: 4.50
+    }
+  ];
+}
 
-function caclSumInConditon() {};
+function loadPromotions() {
+  return [
+    {
+      type: 'BUY_TWO_GET_ONE_FREE',
+      barcodes: [
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
+      ]
+    }
+  ];
+}
+
+//统计商品
+function calculation(barcodeArray) {
+    let uniqueArray = unique(barcodeArray)
+    let resultArray = []
+    for (let i = 0; i < uniqueArray.length; i++) {
+        let itemObj = {
+            barcode:uniqueArray[i], 
+            num:0
+        }
+        for (let j = 0; j < barcodeArray.length; j++) {
+            let index = barcodeArray[j].indexOf('-')
+            if(uniqueArray[i]===barcodeArray[j] || uniqueArray[i]===barcodeArray[j].substring(0,index)){
+                if(index > -1) {
+                    itemObj.num = itemObj.num + parseFloat(barcodeArray[j].substring(index+1))
+                } else {
+                    itemObj.num = itemObj.num + 1
+                }
+            }
+        }
+        resultArray.push(itemObj)
+    }
+    return resultArray
+}
+// 数组去重
+function unique(Array){
+    const s = new Set()
+    let tempArray = Array.concat()
+    for(var i = 0; i <tempArray.length;i++){
+        if(tempArray[i].includes('-')){
+            let index = tempArray[i].indexOf('-')
+            tempArray[i] = tempArray[i].substring(0,index)
+        }
+    }
+    tempArray.forEach(x => s.add(x));
+    let uniqueArray = [...s]
+    return uniqueArray
+}
+//生成商品列表清单
+function matchProductInfo(calculationArray){
+    let AllGoodsList = loadAllItems()
+    for(let i of calculationArray){
+        for(let j of AllGoodsList){
+            if(i.barcode === j.barcode) {
+                i = Object.assign(i,j);
+                i.productDiscoultSum = countItemSum(i,0)
+                i.productNormalSum = countItemSum(i,1)
+            }
+        }
+    }
+    return calculationArray
+}
+//计算某个商品总价
+function countItemSum(itemObj,key){ // key:0,表示计算折扣价格，key：1，计算正常价格
+    let PromotionsObj = loadPromotions()
+    let PromotionsArray = PromotionsObj[0].barcodes
+    let productSum = 0
+    if(key ===0){
+        if (PromotionsArray.indexOf(itemObj.barcode) > -1){
+            productSum = (itemObj.num - parseInt(itemObj.num/3)) * itemObj.price
+        } else {
+            productSum = itemObj.num * itemObj.price
+        }
+    } else if(key ===1){
+        productSum = itemObj.num * itemObj.price
+    }
+    return productSum
+}
+//计算清单总价格
+function calculateTotalListPriceMoney(productList, key){ // key:0,表示计算折扣价格，key：1，计算正常价格
+    let sum = 0
+    for (let obj of productList){
+        if(key===0){
+            sum+= obj.productDiscoultSum
+        } else {
+            sum+= obj.productNormalSum
+        }
+    }
+    return sum
+}
+// 生成清单信息
+    function buildReceiptMsg(goodsList,discountTotalMoney,saveMoney){
+        let receiptListInfo = ''
+        for(let obj of goodsList) {
+            receiptListInfo = receiptListInfo + `\n  名称：${obj.name}，数量：${obj.num}${obj.unit}，单价：${obj.price.toFixed(2)}(元)，小计：${obj.productDiscoultSum.toFixed(2)}(元)`
+        }
+        let receiptMsg = `***<没钱赚商店>收据***${receiptListInfo}
+  ----------------------
+  总计：${discountTotalMoney.toFixed(2)}(元)
+  节省：${saveMoney.toFixed(2)}(元)
+  **********************`
+        return receiptMsg
+    }
+// 输出清单函数
+function printReceipt(productArray){
+    let calculationArray = calculation(productArray) // 统计后的数组
+    let goodsList = matchProductInfo(calculationArray) // 信息匹配和计算总价后的数组
+    let normalTotalMoney = calculateTotalListPriceMoney(goodsList, 1) //计算正常总价格
+    let discountTotalMoney = calculateTotalListPriceMoney(goodsList, 0) //计算打折总价格
+    let saveMoney = normalTotalMoney - discountTotalMoney // 打折后的价格
+    let receiptMsg = buildReceiptMsg(goodsList,discountTotalMoney,saveMoney) // 生成清单信息
+    console.log(receiptMsg)
+}
+  
 
 module.exports = {
-    caclRemaindar,
-    caclSum,
-    caclSumInConditon
+    calculation,
+    unique,
+    matchProductInfo,
+    countItemSum,
+    calculateTotalListPriceMoney,
+    buildReceiptMsg,
+    printReceipt
 }
--- node-jasmines-basic-stack/spec/main-spec.js
+++ node-jasmines-basic-stack/spec/main-spec.js
@@ -1,26 +1,148 @@
 const main = require('../main/main');
-
+const {calculation,unique,matchProductInfo,countItemSum,calculateTotalListPriceMoney,buildReceiptMsg,printReceipt} = require('../main/main');
 describe('main()', () => {
 
-    it('should calculate the remaindar', () => {
-        expect(main.caclRemaindar(9, 3)).toBe(0);
-        expect(main.caclRemaindar(11, 4)).toBe(3);
-    });
+    //测试数组去重unique（）
+  it('you pass unique test', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const uniqueArray = JSON.stringify(unique(tags))
+    const expectText = JSON.stringify(['ITEM000001', 'ITEM000003', 'ITEM000005']);
+    expect(uniqueArray).toBe(expectText);
+  });
 
-    it('should calculate the sum of numbers which are in array', () => {
-        let numArray1 = [3, 6, 1, 8, 6, 4, 2];
-        let numArray2 = [1, 2, 6, 2, 9, 0, 4];
-        
-        expect(main.caclSum(numArray1)).toBe(30);
-        expect(main.caclSum(numArray2)).toBe(24);
-    });
+  // 统计商品测试calculation()
+  it('you pass calculation test', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001-3.8',
+      'ITEM000001',
+      'ITEM000003-6.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const calculationArray = JSON.stringify(calculation(tags))
+    const expectText = JSON.stringify(
+      [
+        { barcode: 'ITEM000001', num: 7.8 },
+        { barcode: 'ITEM000003', num: 6.5 },
+        { barcode: 'ITEM000005', num: 3 }]
+    );
+    expect(calculationArray).toBe(expectText);
+  });
 
-    it('should calculate the sum of numbers which are in array and less than indicated num ', () => {
-        let numArray1 = [3, 6, 1, 8, 6, 4, 2];
-        let numArray2 = [1, 2, 6, 2, 9, 0, 4];
-        
-        expect(main.caclSumInConditon(numArray1 , 5)).toBe(10);
-        expect(main.caclSumInConditon(numArray2 , 6)).toBe(9);
+  //生成商品清单测试matchProductInfo()
+  it('you pass matchProductInfo test', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001-3',
+      'ITEM000001',
+      'ITEM000003-6.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const matchProductInfoArray = JSON.stringify(matchProductInfo(calculation(tags)))
+    const expectText = JSON.stringify(
+      [
+        {
+          "barcode": "ITEM000001",
+          "num": 7,
+          "name": "雪碧",
+          "unit": "瓶",
+          "price": 3,
+          "productDiscoultSum": 15,
+          "productNormalSum": 21,
+        },
+        {
+          "barcode": "ITEM000003",
+          "num": 6.5,
+          "name": "荔枝",
+          "unit": "斤",
+          "price": 15,
+          "productDiscoultSum": 97.5,
+          "productNormalSum": 97.5,
+        },
+        {
+          "barcode": "ITEM000005",
+          "num": 3,
+          "name": "方便面",
+          "unit": "袋",
+          "price": 4.5,
+          "productDiscoultSum": 9,
+          "productNormalSum": 13.5,
+        }]
+    );
+    expect(matchProductInfoArray).toBe(expectText);
+  });
+  
+  // 计算小计函数测试countItemSum（）
+  it('you pass countItemSum test', () => {
+    const tags = {
+      "barcode": "ITEM000001",
+      "num": 7,
+      "price": 3
+    }
+    const itemSum = countItemSum(tags,0)
+    const expectText = 15;
+    expect(itemSum).toBe(expectText);
+  });
+
+  // 计算清单总价函数测试countItemSum（）
+  it('you pass countItemSum test', () => {
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001-3',
+      'ITEM000001',
+      'ITEM000003-6.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+    const itemSum = calculateTotalListPriceMoney(matchProductInfo(calculation(tags)), 0)
+    const expectText = 121.5;
+    expect(itemSum).toBe(expectText);
+  });
+  // 计算清单总价函数测试printReceipt（）
+    it('you pass printReceipt test', () => {
+
+      const tags = [
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000003-2.5',
+        'ITEM000005',
+        'ITEM000005-2',
+      ];
+
+      spyOn(console, 'log');
+
+      printReceipt(tags);
+
+      const expectText = `***<没钱赚商店>收据***
+  名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+  名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+  名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+  ----------------------
+  总计：58.50(元)
+  节省：7.50(元)
+  **********************`;
+
+      expect(console.log).toHaveBeenCalledWith(expectText);
     });
 
 });
